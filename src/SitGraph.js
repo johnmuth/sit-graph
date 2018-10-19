afeb08d86c236868447bf2258457fc7095fbe30d
John Muth <johnmuthuk@gmail.com>
2018-10-19 09:20:37 +0100

    basically working

--- src/SitGraph.js
+++ src/SitGraph.js
@@ -10,16 +10,23 @@ class SitGraph extends Component {
   getElements() {
     return {
       nodes: [
-        { data: { id: 'a', parent: 'b' }, position: { x: 215, y: 85 } },
-        { data: { id: 'b' } },
-        { data: { id: 'c', parent: 'b' }, position: { x: 300, y: 85 } },
-        { data: { id: 'd' }, position: { x: 215, y: 175 } },
-        { data: { id: 'e' } },
-        { data: { id: 'f', parent: 'e' }, position: { x: 300, y: 175 } }
+        { data: { id: 'a' } },
+        { data: { id: 'b' } }
       ],
       edges: [
-        { data: { id: 'ad', source: 'a', target: 'd' } },
-        { data: { id: 'eb', source: 'e', target: 'b' } }
+        { data: { id: 'ab', source: 'a', target: 'b' } }
+      ],
+      style:[
+        {
+          selector: 'edge',
+          style: {
+            'width': 4,
+            'target-arrow-shape': 'triangle',
+            'line-color': '#9dbaea',
+            'target-arrow-color': '#9dbaea',
+            'curve-style': 'bezier'
+          }
+        }
       ]
     };
   }
